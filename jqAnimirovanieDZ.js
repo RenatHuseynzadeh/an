953a76dd9dda48c710a41443e6a3ac1c1f70158d
RenatHuseynzadeh <124487433+RenatHuseynzadeh@users.noreply.github.com>
2023-10-03 19:03:51 +0300

    Add files via upload

---
+++ jqAnimirovanieDZ.js
@@ -0,0 +1,96 @@
+
+// 1
+// $('.btn').click(function(){
+    // $('.slide').slideDown()
+// })
+
+// 2
+// $('.btn').click(function(){
+    // $('.slide').slideUp()
+// })
+
+// 3
+// $('.btn').click(function(){
+    // $('.slide').slideToggle()
+// })
+
+// 4
+// $('.btn').click(function(){
+    // $('.slide').slideDown(4000)
+// })
+
+// 5
+// $('.btn').click(function(){
+    // $('.slide').slideUp(5000)
+// })
+
+// 6
+// $('.btn').click(function(){
+    // $('.slide').slideToggle(6000)
+// })
+
+// 7
+//  $('.btn').click(function(){
+    //  $('.slide').slideUp(5000, function(){
+    //   alert('Вверх')
+        // 
+//  
+    //  })
+//    
+//  })
+
+// 8
+//  $('.btn').click(function(){
+    //  $('.slide').slideDown(5000, function(){
+    //   alert('Вниз')
+        // 
+//  
+    //  })
+//    
+//  })
+
+// 9
+//  $('.btn').click(function(){
+    //  $('.slide').slideToggle(5000, function(){
+    //   alert('Движение')
+        // 
+//  
+    //  })
+//    
+//  })
+
+// 10
+//   $('.btn').click(function(){
+    // $('.slide').animate({
+            //  
+            //  marginLeft: '100px'
+        //  }, 2000)
+        //  
+//   
+    //   
+    // 
+//   })
+
+// 11
+// $('.btn').click(function(){
+    // $('.slide').animate({
+        // width: '+=100px',
+        //  height: '+=100px'
+        //  }, 2000)
+        //  
+//   
+    //   
+    // 
+//   })
+
+// 12
+// что-бы остоновить действующую анимацию
+
+// 13
+//true,false 
+
+// 14
+// что-бы в JQ задать стили
+
+// 15
+// во первых не думаю что стоит делать слайд даун и ап для P и что-бы сделать сначало слайдАП потом сразу слайдДаун,делается не так
